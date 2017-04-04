0c425aace9403e6d2d14a3160a22813250f488a7
Stranger in the Q <tripmannn@gmail.com>
2017-04-04 09:48:09 +0300

    refactor

---
+++ src/builder.js
@@ -0,0 +1,72 @@
+var gulp = require('gulp'),
+var pump = require('pump')
+var plugins = require('./plugins');
+
+var config = {
+    src: './src/',
+    dest: './bin/',
+    temp: './temp/'
+};
+
+module.exports = {
+    core: core,
+    plugins: plugins,
+    config: config,
+    task: createTaskBuilder
+};
+
+function createTaskBuilder(name) {
+
+    if (!name) {
+        throw new Error('task must be named');
+    }
+
+    var dependencies = [];
+    var tasks = [];
+
+    var builder = {
+        subTask: subTask
+    }
+
+    builder.depends = function (deps) {
+        dependencies = deps;
+        return builder;
+    };
+
+    builder.pump = function () {
+        return gulp.task(name, dependencies, pumpCallback);
+    };
+
+    builder.src = function (path) {
+        return subTask(gulp.src(srcFromPath(path)));
+    };
+
+    builder.temp = function () {
+        return subTask(gulp.dest(config.temp)).pump();
+    };
+
+    builder.dest = function (path) {
+        return subTask(gulp.dest(path ? path : config.dest)).pump();
+    };
+
+    return builder;
+
+    function subTask(task) {
+        tasks.push(task);
+        return builder;
+    }
+}
+
+function pumpCallback(callback) {
+    pump(builder.tasks, callback);
+}
+
+function srcFromPath (path) {
+    if (typeof path == "string") {
+        return config.src + (path || '');
+    } else {
+        return path.map(function (src) {
+            return config.src + src;
+        });
+    }
+}
---
+++ src/plugins.js
@@ -0,0 +1,54 @@
+var plugins = {
+     webpack: require('webpack-stream'),
+     concat: require('gulp-concat'),
+     concatCss: require('gulp-concat-css'),
+     minifyCss: require('gulp-cssnano'),
+     htmlMin: require("gulp-htmlmin"),
+     rename: require("gulp-rename"),
+     webServer: require('gulp-webserver'),
+     fileInclude: require('gulp-file-include'),
+     removeEmptyLines: require('gulp-remove-empty-lines'),
+     stylus: require('gulp-stylus'),
+};
+
+module.exports = plugins;
+
+plugins.extend = function(builder) {
+     builder.webpack = function(c, uglify) {
+         return builder.subTask(plugins.webpack(webpackConfig(c, uglify)));
+     };
+     builder.concatCss = function(filename) {
+         return builder.subTask(plugins.concatCss(filename));
+     };
+     builder.fileInclude = function () {
+         return builder.subTask(plugins.fileInclude());
+     };
+     builder.stylus = function() {
+         return builder.subTask(plugins.stylus());
+     };
+     builder.removeEmptyLines = function() {
+         return builder.subTask(plugins.removeEmptyLines());
+     };
+     builder.minifyCss = function() {
+         return builder.subTask(plugins.minifyCss());
+     };
+}
+
+function webpackConfig(c, uglify) {
+
+    var cfg = typeof c == "string" ? {
+        entry: config.src + c,
+        output: {
+            filename: c.split("/").pop()
+        }
+    } : config;
+
+    if (uglify) {
+        if (!cfg.plugins) {
+            cfg.plugins = [];
+        }
+        cfg.plugins.push(new plugins.webpack.webpack.optimize.UglifyJsPlugin({ minimize: true }));
+    }
+
+    return cfg;
+}
