6db22a47640c6bebc3602b2107f20bfe72847015
Balashev Konstantin <balashev.konstantin@nicetu.spb.ru>
2017-04-05 10:12:32 +0300

    refactor

--- src/plugins.js
+++ src/plugins.js
@@ -4,9 +4,9 @@ var plugins = {
     webpack: require('webpack-stream'),
     concatCss: require('gulp-concat-css'),
     minifyCss: require('gulp-cssnano'),
+    fileInclude: require('gulp-file-include'),
     removeEmptyLines: require('gulp-remove-empty-lines'),
     stylus: require('gulp-stylus'),
-    fileInclude: require('gulp-file-include'),
     concat: require('gulp-concat'),
     htmlMin: require("gulp-htmlmin"),
     rename: require("gulp-rename"),
@@ -15,22 +15,45 @@ var plugins = {
 
 module.exports = plugins;
 
-plugins.extend = function(builder) {
+plugins.extend = function (builder) {
 
-     builder.webpack = function(c, uglify) {
-         return builder.subTask(plugins.webpack(webpackConfig(c, uglify, builder)));
-     };
+    builder.webpack = function (c, uglify) {
+        return builder.subTask(plugins.webpack(webpackConfig(c, uglify, builder)));
+    };
 
-     for (var plugin in plugins) {
-         if (plugins.hasOwnProperty(plugin) && plugin !== 'webpack') {
-             var func = plugins[plugin];
-             builder[plugin] = function (settings) {
-                 return builder.subTask(func(settings));
-             }
-         }
-     }
+    builder.stylus = function () {
+        return builder.subTask(plugins.stylus());
+    };
 
-     return builder;
+    builder.concatCss = function (filename) {
+        return builder.subTask(plugins.concatCss(filename));
+    };
+
+    builder.fileInclude = function () {
+        return builder.subTask(plugins.fileInclude());
+    };
+
+    builder.concat = function (filename) {
+        return builder.subTask(plugins.concat(filename));
+    };
+
+    builder.removeEmptyLines = function () {
+        return builder.subTask(plugins.removeEmptyLines());
+    };
+
+    builder.minifyCss = function () {
+        return builder.subTask(plugins.minifyCss());
+    };
+
+    builder.minifyHtml = function () {
+        return builder.subTask(plugins.htmlMin());
+    };
+
+    builder.serve = function (cfg) {
+        return builder.subTask(plugins.webServer(cfg));
+    };
+
+    return builder;
 };
 
 function webpackConfig(c, uglify, builder) {
